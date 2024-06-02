2609143341282c69255193defcac87af2c01cce6
Dmytro <dmytro.zhmur@nure.ua>
2024-06-02 20:52:29 +0300

    Added edge service

---
+++ edge-service-api/src/main/java/ua/nure/edgeserviceapi/EdgeServiceApiApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.edgeserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApiApplication.class, args);
+	}
+
+}
---
+++ edge-service-api/src/test/java/ua/nure/edgeserviceapi/EdgeServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nure.edgeserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdgeServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
